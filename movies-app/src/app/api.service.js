ea8fc3ea83a595c46a5564d4ef07a3dd30858ac9
kayo <kayomarz@gmail.com>
2019-06-21 15:55:11 +0530

    api: compose image urls.

--- movies-app/src/app/api.service.js
+++ movies-app/src/app/api.service.js
@@ -1,5 +1,8 @@
 import {API_KEY} from './api-key';
 
+const BASE_URI = 'https://api.themoviedb.org';
+const BASE_URI_IMAGE = 'https://image.tmdb.org/t/p/w500/';
+
 export const api = [
   '$http',
   function ($http) {
@@ -10,16 +13,25 @@ export const api = [
       return getUrl('/trending/movie/week');
     }
     function getUrl(_url) {
-      const url = `https://api.themoviedb.org/3${_url}`;
+      const url = `${BASE_URI}/3${_url}`;
       return $http.get(url, {
         params: Object.assign({}, defaults)
-      // }).then(result => {
-      //   console.log(`*** ${_url}`, result);
-      //   return result;
-      });
+      }).then(result => {
+        console.log(`*** ${_url}`, result);
+        return result;
+      }).then(response => response.data.results.map(mapToMovieCard));
     }
     return {
       getUrl,
       trendingMovies
     };
   }];
+
+function mapToMovieCard(movie) {
+  return {
+    title: movie.title,
+    genre: movie.genre_ids,
+    rating: Math.round(movie.vote_average / 2),
+    image: `${BASE_URI_IMAGE}${movie.poster_path}`
+  };
+}
